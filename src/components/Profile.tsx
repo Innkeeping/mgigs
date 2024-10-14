9ef055a8e816bea8e152abc318b9af923b2aa0bf
innkeeping <innkeeping@metagame.wtf>
2024-10-14 01:03:10 -0500

    sidebar/profile card adjustments ⚙

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -4,7 +4,7 @@ const Profile = () => {
   return (
     <Sidebar>
       <h1 className="text-4xl font-bold mb-4 mt-16">My Profile</h1>
-      <div className="max-w-2xl mx-auto grid grid-cols-1 lg:grid-cols-2 gap-4 mt-24 lg:mt-16 ml-14 lg:mr-4">
+      <div className="max-w-2xl mx-auto grid grid-cols-1 lg:grid-cols-2 gap-4 mr-4 mt-20 lg:mt-16 ml-14 lg:mr-4 ">
         <div className="justify-center">
           <div className="card bg-base-100 shadow-xl w-full lg:w-48 h-fit">
             <div className="card-body items-center">
@@ -32,13 +32,13 @@ const Profile = () => {
           </div>
         </div>
         <div className="lg:col-span-1">
-          <div className="card bg-base-100 shadow-xl w-full h-96">
+          <div className="card bg-base-100 shadow-xl w-full h-fit">
             <div className="card-body">
               <h2 className="card-title">About Me</h2>
               <p className="text-sm text-gray-400">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet nulla auctor, vestibulum magna sed, convallis ex.</p>
             </div>
           </div>
-          <div className="card bg-base-100 shadow-xl w-full h-96 mt-2">
+          <div className="card bg-base-100 shadow-xl w-full h-fit mt-2">
             <div className="card-body">
               <h2 className="card-title">Recent Gigs</h2>
               <p className="text-sm text-gray-400">List of recent gigs</p>
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,21 +2,37 @@ const Sidebar = ({ children }) => {
   return (
     <div className="drawer lg:drawer-open">
       <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-      <div className="drawer-content flex flex-col items-center justify-center">
+      <div className="drawer-content flex flex-col items-center justify-center overflow-y-hidden">
         {children}
         <label htmlFor="my-drawer-2" className="btn btn-square btn-md btn-neutral w-8 h-8 fixed top-24 left-0 z-10 lg:hidden">
           →
         </label>
       </div>
-      <div className="drawer-side mt-20 lg:mt-16">
+      <div className="drawer-side mt-20 lg:mt-16 lg:ml-14">
         <label htmlFor="my-drawer-2" aria-label="close sidebar" className="drawer-overlay lg:hidden"></label>
-        <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4 ml-14">
-          <li><a>🔍Discover</a></li>
-          <li><a>▦Taskfeed</a></li>
-          <li><a>🔖Bookmarks</a></li>
-          <li><a>🔔Inbox</a></li>
-          <li><a>👤Profile</a></li>
-        </ul>
+        <div className="flex flex-col h-screen pt-8">
+          <ul className="menu bg-base-200 text-base-content">
+            <li><a>🔍Discover</a></li>
+            <li><a>▦Taskfeed</a></li>
+            <li><a>🔖Bookmarks</a></li>
+            <li><a>🔔Inbox</a></li>
+            <li><a>👤Profile</a></li>
+          </ul>
+          <div className="bg-base-200 p-4 border-t border-base-300">
+            <div className="flex items-center">
+              <div className="avatar mr-4">
+                <div className="w-8 rounded-full">
+                  <img src="src/assets/laptop.svg" className="rounded-full" />
+                </div>
+              </div>
+              <span className="text-lg font-bold">User Name</span>
+            </div>
+            <div className="flex items-center mt-4">
+              <button className="btn btn-square btn-xs btn-neutral mr-4">⚙️</button>
+              <button className="btn btn-square btn-xs btn-neutral">🔔</button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
